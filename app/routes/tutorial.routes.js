9443576f6c094a74dd704acccdcbbe99999aa9b2
cleroux84 <cleroux84@gmail.com>
2022-02-21 13:50:08 +0100

    jwt on event

--- app/routes/tutorial.routes.js
+++ app/routes/tutorial.routes.js
@@ -52,8 +52,8 @@ module.exports = app => {
 //CRUD appointments
     app.post("/api/appointment", appointment.create)
     app.get("/api/appointment", appointment.findAll);
-    app.get("/api/appointment/:id", /*checkJwt,*/ appointment.findOne);
-    app.delete("/api/appointment/:id", appointment.delete);
+    app.get("/api/appointment/:id", checkJwt, appointment.findOne);
+    app.delete("/api/appointment/:id", checkJwt, appointment.delete);
     app.put("/api/appointment/:id",checkJwt, appointment.update);
 
 //gestion des images
