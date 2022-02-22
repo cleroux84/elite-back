5f00d8a1eea0786ef267af2e334eab25a1eaf6c3
cleroux84 <cleroux84@gmail.com>
2022-02-22 09:34:12 +0100

    db

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,7 +2,7 @@ module.exports = {
     HOST: "us-cdbr-east-05.cleardb.net",
     USER: "b9581f613adf3c",
     PASSWORD: "b775d398",
-    DB: "heroku_dbb3b36301fcc",
+    DB: "heroku_dbb3b36301fccda",
     dialect: "mysql",
     pool: {
         max: 5,
