1b3dc8c89208b74a7fdd935870f79a925aeb0115
cleroux84 <cleroux84@gmail.com>
2022-02-22 09:15:33 +0100

    m

--- server.js
+++ server.js
@@ -25,6 +25,6 @@ app.get("/", (req, res) => {
     res.json({ message: "Welcome to cleroux84 application." });
 });
 require("./app/routes/tutorial.routes")(app);
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`))
