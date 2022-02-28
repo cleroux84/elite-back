e5324ffed321dff8b28e0d6e99ae88a30a909804
cleroux84 <cleroux84@gmail.com>
2022-02-28 15:29:13 +0100

    refacto

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
-const multer = require('multer');
+
 const app = express();
 
 const corsOptions = {
-    // origin: "http://localhost:8081"
-    origin: "https://preprodelitecoaching42.herokuapp.com"
+    origin: "http://localhost:8080"
+    // origin: "https://preprodelitecoaching42.herokuapp.com"
 };
 app.use(cors(corsOptions));
 // parse requests of content-type - application/json
@@ -17,39 +15,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // app.use("./uploads", express.static(__dirname + './uploads'));
-
-require('dotenv').config();
-cloudinary.config({
-    cloud_name: process.env.CLOUDINADRY_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
-const storage = new CloudinaryStorage({
-    cloudinary: cloudinary,
-    // params: {
-    //     destination: function (req, file, cb) {
-    //         cb(null, './uploads/')
-    //     },
-    //     filename: function(req, file, cb) {
-    //         cb(null, file.originalname)
-    //     }
-    // }
-})
-const parser = multer({storage})
-app.post("/upload", parser.single('file'), async (req, res) => {
-    try{
-        const result = await cloudinary.uploader.upload(req.file.path, {
-            upload_preset: 'dev_setups'
-        }).then(response => {
-            res.send({
-                message: response.secure_url
-            })
-        })
-    } catch (err) {
-        console.log(err)
-    }
-})
 
 const db = require("./app/models");
 //drop the table if already exists
