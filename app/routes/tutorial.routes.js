cb7430d4659d63fa86216f3fd2f6f6bf53a8fdc8
cleroux84 <cleroux84@gmail.com>
2022-02-28 16:47:06 +0100

    securize

--- app/routes/tutorial.routes.js
+++ app/routes/tutorial.routes.js
@@ -2,23 +2,21 @@ const path = require("path");
 const jwt = require("express-jwt");
 const jwksRsa = require("jwks-rsa");
 //auth0 API
-const authConfig = {
-    domain: "dev-nrug8pbx.us.auth0.com",
-    audience: "https://elite-coaching-api.com"
-};
+
 const articles = require("../controllers/tutorial.controller");
 const admin = require('../controllers/admin.controller');
 const appointment = require('../controllers/appointment.controller');
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const multer = require('multer');
+const Process = require("process");
 require('dotenv').config();
 
 module.exports = app => {
     cloudinary.config({
-        cloud_name: "hghzq1rcq",
-        api_key: "393961743377671",
-        api_secret: "hjTBFcQqfCu1m0O6pYiS1V0_Xq8",
+        cloud_name: Process.env.CLOUDINARY_NAME,
+        api_key: Process.env.CLOUDINARY_API_KEY,
+        api_secret: Process.env.CLOUDINARY_API_SECRET,
     });
 
     const storage = new CloudinaryStorage({
@@ -35,7 +33,10 @@ module.exports = app => {
         res.json(req.file)
         console.log(req.file)
     })
-
+    const authConfig = {
+        domain: Process.env.AUTH_DOMAIN,
+        audience: Process.env.AUTH_AUDIENCE
+    };
     const checkJwt = jwt({
         // Provide a signing key based on the key identifier in the header and the signing keys provided by your Auth0 JWKS endpoint.
         secret: jwksRsa.expressJwtSecret({
