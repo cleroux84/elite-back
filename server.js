13a2f0247fe841f91eb52bc8cf631c27ae6f2d3e
cleroux84 <cleroux84@gmail.com>
2022-02-28 15:34:52 +0100

    port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 const corsOptions = {
-    // origin: "http://localhost:8080"
+    // origin: "http://localhost:8081"
     origin: "https://preprodelitecoaching42.herokuapp.com"
 };
 app.use(cors(corsOptions));
