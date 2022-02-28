1be9ea2cd61bff79e0a8cd2229dbc6362a2801ad
cleroux84 <cleroux84@gmail.com>
2022-02-28 20:21:52 +0100

    gitignore

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -11,16 +11,3 @@ module.exports = {
         idle: 10000
     },
 };
-/*module.exports = {
-    HOST: "127.0.0.1",
-    USER: "root",
-    PASSWORD: "Sasha",
-    DB: "elite42",
-    dialect: "mysql",
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    },
-};*/
